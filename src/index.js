248e21a47fb1d19d645bbdfca3867452c079f3ac
Taylor Ng <taylormng95@gmail.com>
2024-05-16 21:37:22 -0400

    completes fetch forms lab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    
+    inputForm.addEventListener('submit', event => {
+        event.preventDefault();
+        const idValue = document.querySelector('#searchByID').value;
+        
+        fetch(`http://localhost:3000/movies/${idValue}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            document.querySelector('#movieDetails h4').textContent = data.title;
+            document.querySelector('#movieDetails p').textContent = data.summary;
+        })
+
+    })
+
 }
 
 document.addEventListener('DOMContentLoaded', init);
